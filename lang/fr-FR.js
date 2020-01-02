8739107b6c51b9dacb70433f88c3f17296160e44
Mohamed amine Herrat <mherrat@e1r2p12.1337.ma>
2020-01-02 19:19:32 +0100

    Deleted

--- lang/fr-FR.js
+++
@@ -1,8 +0,0 @@
-module.exports= {
-    welcome: 'Bienvenue sur Hypertube!',
-    welcomemsg: 'Vous pouvez vous connectez en fournissons votre login et votre mot de pass, si vous avez pas encore creer un compte n`hesiter pas a s`inscrire',
-    pages: {
-      home: 'Page initial',
-      contacts: 'contacts'
-    },
-  }
